60d7e32fe1116d79f0eb7beee4b8af88019c06f8
RaulSilveira <raul95@gmail.com>
2019-09-28 09:41:44 -0300

    Compile file organization

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 # Compilation script for SystemC on MacOS X 
+
 g++ -std=c++11 -o bin/exe src/main.cpp src/mux.cpp src/router.cpp src/core.cpp src/manager.cpp -I$SYSTEMC_HOME/include -L$SYSTEMC_HOME/lib-macosx64 -lsystemc -lm
